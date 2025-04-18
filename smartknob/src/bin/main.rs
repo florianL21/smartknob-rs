b392e1595056770f7366417e04aad5dd7eeec14b
Florian Laschober <6053565+florianL21@users.noreply.github.com>
2025-04-18 17:47:13 +0200

    Encoder working with dirty hack

--- smartknob/src/bin/main.rs
+++ smartknob/src/bin/main.rs
@@ -1,13 +1,16 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
 use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
     clock::CpuClock,
     dma::{DmaRxBuf, DmaTxBuf},
     dma_buffers,
+    gpio::{GpioPin, Level, Output, OutputConfig},
     rmt::Rmt,
     spi::{
         master::{Config, Spi},
@@ -16,18 +19,48 @@ use esp_hal::{
     time::Rate,
     timer::systimer::SystemTimer,
 };
+use mt6701::{self, AngleSensorTrait};
 
-// use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
 use log::info;
-// use mt6701::AngleSensorTrait;
 use smart_leds::{
     brightness,
     colors::{BLACK, RED},
     gamma, SmartLedsWrite,
 };
 
-extern crate alloc;
+#[embassy_executor::task]
+async fn log_angle(
+    spi_bus: esp_hal::spi::master::SpiDmaBus<'static, esp_hal::Async>,
+    pin_mag_csn: GpioPin<12>,
+) {
+    let spi_bus_mutex: Mutex<
+        NoopRawMutex,
+        esp_hal::spi::master::SpiDmaBus<'static, esp_hal::Async>,
+    > = embassy_sync::mutex::Mutex::new(spi_bus);
+
+    let spi_device = SpiDevice::new(
+        &spi_bus_mutex,
+        Output::new(pin_mag_csn, Level::Low, OutputConfig::default()),
+    );
+
+    let mut encoder: mt6701::MT6701Spi<
+        SpiDevice<
+            '_,
+            NoopRawMutex,
+            esp_hal::spi::master::SpiDmaBus<'_, esp_hal::Async>,
+            Output<'_>,
+        >,
+    > = mt6701::MT6701Spi::new(spi_device);
+    loop {
+        if encoder.update(0).await.is_ok() {
+            let pos = encoder.get_position();
+            info!("pos: {}", pos);
+        }
+        Timer::after_millis(200).await;
+    }
+}
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -42,6 +75,7 @@ async fn main(spawner: Spawner) {
 
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
+    info!("Embassy initialized!");
 
     // Pins for LDC1614
     // let pins_ldc_int_pin = peripherals.GPIO40;
@@ -76,7 +110,7 @@ async fn main(spawner: Spawner) {
     let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
     let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
 
-    let mut spi_bus = Spi::new(
+    let spi_bus: esp_hal::spi::master::SpiDmaBus<'_, esp_hal::Async> = Spi::new(
         peripherals.SPI2,
         Config::default()
             .with_frequency(Rate::from_khz(100))
@@ -85,20 +119,12 @@ async fn main(spawner: Spawner) {
     .unwrap()
     .with_sck(pin_mag_clk)
     .with_miso(pin_mag_do)
-    .with_cs(pin_mag_csn)
     .with_dma(peripherals.DMA_CH0)
     .with_buffers(dma_rx_buf, dma_tx_buf)
     .into_async();
 
-    // let spi_bus_mutex = embassy_sync::mutex::Mutex::new(spi_bus);
-
-    // let spi_device = SpiDevice::new(&spi_bus_mutex, pin_mag_csn);
-
-    // let sensor = mt6701::MT6701Spi::new(spi_device.into());
-
-    info!("Embassy initialized!");
-
     // TODO: Spawn some tasks
+    spawner.spawn(log_angle(spi_bus, pin_mag_csn)).unwrap();
     let _ = spawner;
 
     let mut data;
