c54617ddc02cf21f83a16021364530f673c6c999
Florian Laschober <6053565+florianL21@users.noreply.github.com>
2025-05-22 20:23:21 +0200

    Switched to embedded-cli for the CLI interface

--- smartknob/src/cli.rs
+++ smartknob/src/cli.rs
@@ -1,11 +1,12 @@
-use core::fmt::Write;
+use core::{convert::Infallible};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embedded_cli::{cli::CliBuilder, Command, CommandGroup};
+use embedded_io_async::Read;
 use esp_backtrace as _;
 use esp_hal::{usb_serial_jtag::UsbSerialJtag, Async};
-use menu::asynchronous::{Item, ItemType, Menu, Parameter, Runner};
-use noline::builder::EditorBuilder;
+use ufmt::{derive::uDebug, uwrite};
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, uDebug, Default)]
 pub struct LogToggles {
     pub encoder: bool,
     pub push_events: bool,
@@ -14,43 +15,7 @@ pub struct LogToggles {
 struct Context {
     sender: embassy_sync::watch::Sender<'static, CriticalSectionRawMutex, LogToggles, 4>,
     logging_config: LogToggles,
-}
-
-fn enter_root(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    interface: &mut UsbSerialJtag<'_, Async>,
-    _context: &mut Context,
-) {
-    let _ = writeln!(interface, "In enter_root");
-}
-
-fn exit_root(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    interface: &mut UsbSerialJtag<'_, Async>,
-    _context: &mut Context,
-) {
-    let _ = writeln!(interface, "In exit_root");
-}
-
-fn select_store(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    _item: &Item<UsbSerialJtag<'_, Async>, Context>,
-    args: &[&str],
-    interface: &mut UsbSerialJtag<'_, Async>,
-    _context: &mut Context,
-) {
-    let _ = writeln!(interface, "In select_store. Args = {:?}", args);
-}
-
-fn select_load(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    _item: &Item<UsbSerialJtag<'_, Async>, Context>,
-    args: &[&str],
-    interface: &mut UsbSerialJtag<'_, Async>,
-    _context: &mut Context,
-) {
-    let _ = writeln!(interface, "In select_load. Args = {:?}", args);
-    // info!("In select_load. Args = {:?}", args);
+    interface_open: bool
 }
 
 fn set_log(toggles: &mut LogToggles, channel: &str, state: bool) -> Result<(), ()> {
@@ -66,132 +31,143 @@ fn set_log(toggles: &mut LogToggles, channel: &str, state: bool) -> Result<(), (
     Ok(())
 }
 
-fn log_enable(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    _item: &Item<UsbSerialJtag<'_, Async>, Context>,
-    args: &[&str],
-    interface: &mut UsbSerialJtag<'_, Async>,
-    context: &mut Context,
-) {
-    if set_log(&mut context.logging_config, args[0], true).is_ok() {
-        let _ = writeln!(interface, "Enabled logging for channel {}", args[0]);
-        context.sender.send(context.logging_config.clone());
-    } else {
-        let _ = writeln!(interface, "Invalid channel {}", args[0]);
+
+#[derive(CommandGroup)]
+enum RootGroup<'a> {
+    Base(Base),
+    Logging(Logging<'a>),
+    Storage(Flash<'a>),
+}
+
+#[derive(Command)]
+#[command(help_title = "Basic commands")]
+enum Base {
+    /// Exit the CLI. This will start logging out all enabled channels. To start the CLI again simply press any button
+    Exit
+}
+
+#[derive(Command)]
+#[command(help_title = "Manage Logging output")]
+enum Logging<'a> {
+    /// Enable
+    LogEnable {
+        /// Which logging channel to enable
+        channel: &'a str,
+    },
+    ///
+    LogDisable {
+        /// Which logging channel to disable
+        channel: &'a str,
+    },
+    /// List all available logging channels
+    LogList,
+
+}
+
+impl Logging<'_> {
+    fn handle(self: &Self, cli: &mut embedded_cli::cli::CliHandle<'_, esp_hal::usb_serial_jtag::UsbSerialJtagTx<'static, Async>, core::convert::Infallible>, context: &mut Context) -> Result<(), Infallible> {
+        match self {
+            Logging::LogList => {
+                uwrite!(cli.writer(), "Available logging channels: {:?}", context.logging_config)?;
+            }
+            Logging::LogEnable { channel } => {
+                if set_log(&mut context.logging_config, channel, true).is_ok() {
+                    uwrite!(cli.writer(), "Enabled logging for channel {}", channel)?;
+                    context.sender.send(context.logging_config.clone());
+                } else {
+                    uwrite!(cli.writer(), "Invalid channel {}", channel)?;
+                }
+            }
+            Logging::LogDisable{ channel } => {
+                if set_log(&mut context.logging_config, channel, false).is_ok() {
+                    uwrite!(cli.writer(), "Disabled logging for channel {}", channel)?;
+                    context.sender.send(context.logging_config.clone());
+                } else {
+                    uwrite!(cli.writer(), "Invalid channel {}", channel)?;
+                }
+            }
+        }
+        Ok(())
     }
 }
 
-fn log_disable(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    _item: &Item<UsbSerialJtag<'_, Async>, Context>,
-    args: &[&str],
-    interface: &mut UsbSerialJtag<'_, Async>,
-    context: &mut Context,
-) {
-    if set_log(&mut context.logging_config, args[0], false).is_ok() {
-        let _ = writeln!(interface, "Disabled logging for channel {}", args[0]);
-        context.sender.send(context.logging_config.clone());
-    } else {
-        let _ = writeln!(interface, "Invalid channel {}", args[0]);
+#[derive(Command)]
+#[command(help_title = "Manage values stored in flash")]
+enum Flash<'a> {
+    /// Store a value to flash
+    FlashStore {
+        /// What value to store
+        value: &'a str,
+    },
+
+    /// Load a value from flash
+    FlashLoad,
+}
+
+impl Flash<'_> {
+    fn handle(self: &Self, cli: &mut embedded_cli::cli::CliHandle<'_, esp_hal::usb_serial_jtag::UsbSerialJtagTx<'static, Async>, core::convert::Infallible>, context: &mut Context) -> Result<(), Infallible> {
+        match self {
+            Flash::FlashLoad => {
+                uwrite!(cli.writer(), "Stored value is: ?")?;
+            }
+            Flash::FlashStore  { value } => {
+                uwrite!(cli.writer(), "Saved value to flash: {}", value)?;
+            }
+        }
+        Ok(())
     }
 }
-
-fn log_list_channels(
-    _menu: &Menu<UsbSerialJtag<'_, Async>, Context>,
-    _item: &Item<UsbSerialJtag<'_, Async>, Context>,
-    _args: &[&str],
-    interface: &mut UsbSerialJtag<'_, Async>,
-    context: &mut Context,
-) {
-    let _ = writeln!(interface, "Avaliable logging channels: {:?}", context.logging_config);
-}
-
-const ROOT_MENU: Menu<UsbSerialJtag<'_, Async>, Context> = Menu {
-    label: "root",
-    items: &[
-        &Item {
-            item_type: ItemType::Callback {
-                function: select_store,
-                parameters: &[Parameter::Mandatory {
-                    parameter_name: "value",
-                    help: Some("The value to store"),
-                }],
-            },
-            command: "save",
-            help: Some("Store a value to flash"),
-        },
-        &Item {
-            item_type: ItemType::Callback {
-                function: select_load,
-                parameters: &[],
-            },
-            command: "load",
-            help: Some("Displays the value saved in flash"),
-        },
-        &Item {
-            item_type: ItemType::Menu(&Menu {
-                label: "logging",
-                items: &[
-                    &Item {
-                        item_type: ItemType::Callback {
-                            function: log_enable,
-                            parameters: &[Parameter::Mandatory {
-                                parameter_name: "channel",
-                                help: Some("Which information to log"),
-                            }],
-                        },
-                        command: "enable",
-                        help: Some("Enable logging of a certain channel"),
-                    },
-                    &Item {
-                        item_type: ItemType::Callback {
-                            function: log_disable,
-                            parameters: &[],
-                        },
-                        command: "disable",
-                        help: Some("Disable logging of a certain channel"),
-                    },
-                    &Item {
-                        item_type: ItemType::Callback {
-                            function: log_list_channels,
-                            parameters: &[],
-                        },
-                        command: "list",
-                        help: Some("List all avaliable logging channels"),
-                    },
-                ],
-                entry: None,
-                exit: None,
-            }),
-            command: "logging",
-            help: Some("Configure which information should be logged to the serial console"),
-        },
-    ],
-    entry: Some(enter_root),
-    exit: Some(exit_root),
-};
 
 #[embassy_executor::task]
 pub async fn menu_handler(
-    mut serial: UsbSerialJtag<'static, Async>,
+    serial: UsbSerialJtag<'static, Async>,
     sender: embassy_sync::watch::Sender<'static, CriticalSectionRawMutex, LogToggles, 4>,
 ) {
-    let mut buffer = [0u8; 100];
-    let mut history = [0u8; 200];
-    let Ok(mut editor) = EditorBuilder::from_slice(&mut buffer)
-        .with_slice_history(&mut history)
-        .build_async(&mut serial)
-        .await
-    else {
-        return;
+    let mut context = Context { sender, logging_config: LogToggles::default(), interface_open: false };
+
+    let command_buffer: [u8; 32] = [0; 32];
+    let history_buffer: [u8; 32] = [0; 32];
+    let (mut rx, tx) = serial.split();
+    let builder = CliBuilder::default()
+        .writer(tx)
+        .command_buffer(command_buffer)
+        .history_buffer(history_buffer)
+        .build();
+    let mut cli = if let Ok(cli) = builder {
+        cli
+    } else {
+        return ;
     };
 
-    let mut context = Context { sender: sender, logging_config: LogToggles::default() };
-    let mut r = Runner::new(ROOT_MENU, &mut editor, serial, &mut context).await;
+    let mut buf: [u8; 1] = [0; 1];
     loop {
-        // Upon error assume that we are just not connected to serial and terminate this function
-        if let Err(_) = r.input_line(&mut context).await {
-            return;
+        // read byte from somewhere (for example, uart)
+        let _ = rx.read(&mut buf).await;
+
+        // if the interface is not open open it and skip processing the CLI
+        if context.interface_open == false {
+            context.interface_open = true;
+            continue;
         }
+
+        let _ = cli.process_byte::<RootGroup, _>(
+            buf[0],
+            &mut RootGroup::processor(|cli, command| {
+                match command {
+                    RootGroup::Logging(logging) => {
+                        let _ = logging.handle(cli, &mut context);
+                    }
+                    RootGroup::Storage(storage) => {
+                        let _ = storage.handle(cli, &mut context);
+                    }
+                    RootGroup::Base(base) => match base {
+                        Base::Exit => {
+                            context.interface_open = false;
+                        }
+                    }
+                }
+                Ok(())
+            }),
+        );
     }
 }
